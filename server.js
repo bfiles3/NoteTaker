400e8634b601414cb20225e36a6ea859941c13b3
bfiles3 <65058048+bfiles3@users.noreply.github.com>
2020-07-09 01:13:07 -0400

    Completed function to append new notes to page, linked HTML to display notes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 1004;
+app.listen(PORT);
 
 //variable for arrays
 const notes = [];
@@ -14,24 +15,45 @@ app.use(express.json());
 
 // assets & paths 
 app.use(express.static('public'));
-app.get("/api/notes", function(req,res) {
+app.get("*", function(req,res) {
     res.sendFile(_dirname, "/public/index.html")
 });
+app.get("/notes", function (req,res){
+    res.sendFile(_dirname + "/public/notes.html")
+});
 app.get("/api/notes", function (req,res) {
-    res.sendFile(_dirname + "/db/db.json", "utf8", function (err, data){
+    res.sendFile(_dirname + "/db/db.json")
+});
+
+// creating new notes
+app.post("api/notes", function (req,res) {
+    fs.readFile(_dirname + "/db/db.json", "utf8", function (err, data){
         if (err){
             console.log(err);
         }
         else {
             notes = JSON.parsa(data);
+            notes.forEach(item => ids.push(item.id));
+            if (ids.length === 0){
+                req.body.id = 1;
+            } else {
+                // create note variable to encompass the new note being added to the array
+                let newNote = ids[ids.length-1] + 1;
+                req.body.id = newNote;
+            }
+        notes.push(newNote);
+
+        fs.writeFile(_dirname + "/db/db.json", "utf8", function (err, data){
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.sendFile(_dirname + "/db/db.json");
+                console.log("Note added");
+            }
+
         }
+        )};
 
-        req.body.id 
-    })
-
-});
-
-// creating new notes
-app.post("api/notes", function (req,res){
-    fs.readFile(_dirname + "/db/db.json")
-}
+  });
+}); 
