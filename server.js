d8f7f48d3e7057ff789093c3bf9a8b95acdb3e02
bfiles3 <65058048+bfiles3@users.noreply.github.com>
2020-07-09 01:57:32 -0400

    Fixed the syntax error in writing notes, removed the part of deleting notes causing a problem in code

--- server.js
+++ server.js
@@ -11,70 +11,60 @@ const notes = [];
 const ids = [];
 
 // parse data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // assets & paths 
 app.use(express.static('public'));
-app.get("*", function(req,res) {
+app.get("*", function (req, res) {
     res.sendFile(_dirname, "/public/index.html")
 });
-app.get("/notes", function (req,res){
+app.get("/notes", function (req, res) {
     res.sendFile(_dirname + "/public/notes.html")
 });
-app.get("/api/notes", function (req,res) {
+app.get("/api/notes", function (req, res) {
     res.sendFile(_dirname + "/db/db.json")
 });
 
 // creating new notes
-app.post("api/notes", function (req,res) {
-    fs.readFile(_dirname + "/db/db.json", "utf8", function (err, data){
-        if (err){
+app.post("api/notes", function (req, res) {
+    fs.readFile(_dirname + "/db/db.json", "utf8", function (err, data) {
+        if (err) {
             console.log(err);
         }
         else {
             notes = JSON.parsa(data);
             notes.forEach(note => ids.push(note.id));
-            if (ids.length === 0){
+            if (ids.length === 0) {
                 req.body.id = 1;
             } else {
                 // create note variable to encompass the new note being added to the array
-                let newNote = ids[ids.length-1] + 1;
+                let newNote = ids[ids.length - 1] + 1;
                 req.body.id = newNote;
             }
-        notes.push(newNote);
+            notes.push(newNote);
 
-        fs.writeFile(_dirname + "/db/db.json", "utf8", JSON.stringify(notes), function (err){
-            if (err) {
-                console.log(err);
+            fs.writeFile(_dirname + "/db/db.json", "utf8", JSON.stringify(notes), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.sendFile(_dirname + "/db/db.json");
+                    console.log("Note added");
+                }
+
             }
-            else {
-                res.sendFile(_dirname + "/db/db.json");
-                console.log("Note added");
-            }
+            )
+        };
 
-        }
-        )};
-
-  });
-}); 
+    });
+});
 
 // deleting notes
-app.delete("/api/notes/:id", function (req, res){
-    fs.readFile(_dirname + "/db/db.json", "utf8", function (err, data){
+app.delete("/api/notes/:id", function (req, res) {
+    fs.readFile(_dirname + "/db/db.json", "utf8", function (err, data) {
         if (err) {
             return console.log(err);
-        } else {
-            let notes = JSON.parse(data).filter (note => {
-                if (note.id != search.id) {
-                    return notes
-                }
-            });
-        fs.writeFile(_dirname + "/db/db.json", JSON.stringify(notes), function (err){
-            return console.log(err);
         }
-        });
-
-        }
-    }
+    });
 })
