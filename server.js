798e60e6e054f980820ca67bef45732c682f9c73
bfiles3 <65058048+bfiles3@users.noreply.github.com>
2020-07-09 00:26:25 -0400

    called necessary files from API

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ const PORT = process.env.PORT || 1004;
 // parse data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// assets & paths 
+app.use(express.static('public'));
+app.get("/api/notes", function(req,res) {
+    res.sendFile(_dirname, "/public/index.html")
+});
+app.get("/api/notes", function (req,res) {
+    res.sendFile(_dirname + "/db/db.json")
+
+});
+
+// creating new notes
