22cb9d76c7593b6ebfb0bd3ab20e9699cb16bdd2
bfiles3 <65058048+bfiles3@users.noreply.github.com>
2020-07-09 00:49:04 -0400

    Created arrays for notes and ids and created a function to append specific notes to the note array

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 1004;
 
+//variable for arrays
+const notes = [];
+const ids = [];
 
 // parse data
 app.use(express.urlencoded({extended: true}));
@@ -15,8 +18,20 @@ app.get("/api/notes", function(req,res) {
     res.sendFile(_dirname, "/public/index.html")
 });
 app.get("/api/notes", function (req,res) {
-    res.sendFile(_dirname + "/db/db.json")
+    res.sendFile(_dirname + "/db/db.json", "utf8", function (err, data){
+        if (err){
+            console.log(err);
+        }
+        else {
+            notes = JSON.parsa(data);
+        }
+
+        req.body.id 
+    })
 
 });
 
 // creating new notes
+app.post("api/notes", function (req,res){
+    fs.readFile(_dirname + "/db/db.json")
+}
